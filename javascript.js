a7faca8a89cb211c1ec7072a22b4852836763a1d
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-08 00:54:59 +0100

    Add display functionality to show player choices
    Gives front-user visible feedback about his pick and the computer pick
    as well as who won the round.

--- javascript.js
+++ javascript.js
@@ -8,42 +8,45 @@ function getComputerChoice() {
     return 'invalid';
 }
 
+let winnerText = document.querySelector('#winner');
+
 function playRound(humanChoice) {
     humanChoice = humanChoice.toUpperCase(); // makes human input case-insensitive
     const computerChoice = getComputerChoice();
+    computerText.textContent = computerChoice;
     let humanWin = false;
 
     if (humanChoice === computerChoice) {
-        console.log('A draw!');
+        winnerText.textContent = 'Draw!';
         return 'draw';
     } 
     
     else if (humanChoice === 'PAPER') {
         if (computerChoice === 'ROCK') {
-            console.log('You win! Paper beats Rock.');
+            winnerText.textContent = 'You win! Paper beats Rock.';
             humanWin = true;
         } else if (computerChoice === 'SCISSOR') {
-            console.log('You lose! Scissor beats Paper.');
+            winnerText.textContent = 'You lose! Scissor beats Paper.';
             humanWin = false;
         }
     } 
     
     else if (humanChoice === 'SCISSOR') {
         if (computerChoice === 'PAPER') {
-            console.log('You win! Scissor beats Paper.');
+            winnerText.textContent = 'You win! Scissor beats Paper.';
             humanWin = true;
         } else if (computerChoice === 'ROCK') {
-            console.log('You lose! Rock beats Scissor.');
+            winnerText.textContent = 'You lose! Rock beats Scissor.';
             humanWin = false;
         }
     }
 
     else if (humanChoice === 'ROCK') {
         if (computerChoice === 'PAPER') {
-            console.log('You lose! Paper beats Rock.');
+            winnerText = 'You lose! Paper beats Rock.';
             humanWin = false;
         } else if (computerChoice === 'SCISSOR') {
-            console.log('You win! Rock beats Scissor');
+            winnerText.textContent = 'You win! Rock beats Scissor';
             humanWin = true;
         }
     }
@@ -56,9 +59,23 @@ const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorButton = document.querySelector('#scissor');
 
+let userText = document.querySelector('#user');
+let computerText = document.querySelector('#computer');
 
 
 
-rockButton.addEventListener('click', () => {playRound('rock')});
-paperButton.addEventListener('click', () => {playRound('paper')});
-scissorButton.addEventListener('click', () => {playRound('scissor')});
+rockButton.addEventListener('click', () => {
+    userText.textContent = 'Rock';
+    playRound('rock');
+});
+
+paperButton.addEventListener('click', () => {
+    userText.textContent = 'Paper';
+    playRound('paper');
+});
+
+scissorButton.addEventListener('click', () => {
+    userText.textContent = 'Scissor';
+    playRound('scissor');
+});
+
